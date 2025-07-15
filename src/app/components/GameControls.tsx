7ae9542c5cd04a96d6e670794362d0e82fa88f01
Adrian Brizuela <adrianalebrizuela@gmail.com>
2025-07-14 23:27:38 -0300

    Removed Unused Variables

--- src/app/components/GameControls.tsx
+++ src/app/components/GameControls.tsx
@@ -1,4 +1,4 @@
-import React, { use, useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { GameContext } from "../../context/GameContext";
 import {
   CellType,
